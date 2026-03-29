fa7715d63baf850184cfd2941d2bbc16ba3048fb
student-poojan <poojanp150@gmail.com>
2026-03-29 19:02:51 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import {  Route, Routes } from 'react-router-dom'
-import Home from './Component/Home'
+import Home from './Component/Index'
 import Projects from './Component/Projects'
 import "./PortfolioStyle.css"
 

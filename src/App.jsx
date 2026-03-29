65b24482fbb3074eb513a5f4ec473f47953fe20f
student-poojan <poojanp150@gmail.com>
2026-03-29 18:55:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import {  Route, Routes } from 'react-router-dom'
+import Home from './Component/Home'
+import Projects from './Component/Projects'
+import "./PortfolioStyle.css"
+
+function App() {
+
+
+  return (
+    <>
+     <Home/>
+    
+    </>
+  )
+}
+
+export default App
